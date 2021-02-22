23b69c34521090f76c08b3441034fcba0e2eeb0f
Nguyen Nhat Linh <linhnn@vmms.vn>
2021-02-22 13:43:22 +0700

    Fix: Set default value for session

--- src/app/Http/Middlewares/Locale.php
+++ src/app/Http/Middlewares/Locale.php
@@ -15,6 +15,9 @@ class Locale
      */
     public function handle($request, Closure $next)
     {
+        if(!Session::has('website_language')){
+            Session(['website_language' => config('app.locale')]);
+        }
 
         $language = Session::get('website_language', config('app.locale'));
         // Lấy dữ liệu lưu trong Session, không có thì trả về default lấy trong config
