383a1b63444380409796a8d40702c1524e8af6cd
dev_duybv <duybv@vicdoers.com>
2022-02-09 15:31:38 +0700

    fix: js change language

--- src/resources/js/change-language.js
+++ src/resources/js/change-language.js
@@ -1,33 +1,37 @@
-import $ from 'jquery';
-
 document.addEventListener('DOMContentLoaded', function() {
-    var getflagclass = document.querySelector(".lang.selected>.flag-icon").getAttribute("class");
-    var datavalue = document.querySelector(".lang.selected").getAttribute("data-value");
-    document.querySelector(".current_lang >.lang>.lang-txt").innerText = datavalue;
-    document.querySelector(".current_lang >.lang>.flag-icon ").setAttribute("class", getflagclass);
-    $(".translate_wrapper .current_lang").click(function(e) {
-        e.stopPropagation();
-        $(this).parent().toggleClass("active");
-
-        setTimeout(function() {
-            document.querySelector(".more_lang").classList.toggle("active");
-        }, 5);
-    });
+    if (document.querySelector(".lang.selected") !== null) {
+        var getflagclass = document.querySelector(".lang.selected>.flag-icon").getAttribute("class");
+        var datavalue = document.querySelector(".lang.selected").getAttribute("data-value");
+        document.querySelector(".current_lang >.lang>.lang-txt").innerText = datavalue;
+        document.querySelector(".current_lang >.lang>.flag-icon ").setAttribute("class", getflagclass);
+        var lang = document.querySelector('.more_lang .selected').getAttribute('data-value');
+        document.querySelector('html').setAttribute('lang', lang);
+    }
+    var el_current_lang = document.querySelectorAll(".translate_wrapper .current_lang");
+    for (var i = 0; i < el_current_lang.length; i++) {
+        el_current_lang[i].addEventListener('click', function(e) {
+            e.stopPropagation();
+            this.parentElement.classList.toggle('active')
+            setTimeout(function() {
+                document.querySelector(".more_lang").classList.toggle("active");
+            }, 5);
+        });
+    };
     const url = window.location.origin;
-    $(".more_lang .lang").click(function() {
-        $(this).addClass("selected").siblings().removeClass("selected");
-        document.querySelector(".more_lang").classList.remove("active");
+    var elements = document.querySelectorAll(".more_lang .lang");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', function() {
+            document.querySelector(".more_lang .lang").classList.remove("selected");
+            this.classList.add("selected");
+            document.querySelector(".more_lang").classList.remove("active");
+            var getflagclass = document.querySelector(".more_lang .selected .flag-icon").getAttribute("class");
+            var lang = this.getAttribute("data-value");
+            document.querySelector(".current_lang .lang-txt").innerText = lang;
+            if (document.querySelector(".current_lang img") !== null) {
+                document.querySelector(".current_lang img").setAttribute("class", getflagclass);
+            }
+            location.href = url + "/change-language/" + lang;
+        });
+    };
 
-        var getflagclass = $(this).find(".flag-icon").attr("class");
-        var lang = $(this).attr("data-value");
-
-        document.querySelector(".current_lang .lang-txt").innerText = lang;
-        if (document.querySelector(".current_lang img") !== null) {
-            document.querySelector(".current_lang img").setAttribute("class", getflagclass);
-        }
-        location.href = url + "/change-language/" + lang;
-    });
-    var lang = document.querySelector('.more_lang .selected').getAttribute('data-value');
-    document.querySelector('html').setAttribute('lang', lang);
-    document.querySelector('body').classList.add(lang);
-}, false);
+});
